a20d796e9828c00fa72028325bb1be9b1e1deec1
floodpeak <hongfengzhou@yahoo.com>
2016-09-27 00:25:01 +0800

    loop number and print getNumber for each
    number loop from 1 to 100
    run without fizz or buzz

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+var getNumber = require('./getNumber')
+
 console.log("hello fizz and buzz");
 
 function main() {
-  return true
+  for(var i = 1;i<=100;i++){
+    console.log(getNumber(i));
+  }
 }
 
-module.exports = main
+main()
