175db437b2889f59e16c3b9637f979383e79dcdc
floodpeak <hongfengzhou@yahoo.com>
2016-09-27 00:27:56 +0800

    add unit test about number 3
    should get Fizz but get 3
    unit test failed

--- test/getNumber.test.js
+++ test/getNumber.test.js
@@ -5,4 +5,7 @@ describe('number fizzbuzzed',function(){
   it('should get 1 when it is 1',function(){
     expect(getNumber(1)).to.be.equal(1)
   })
+  it('should get Fizz when it is 3', function(){
+    expect(getNumber(3)).to.be.equal('Fizz')
+  })
 })
